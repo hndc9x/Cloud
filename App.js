d572e6d0cca6e43846965b281f634d161d6be308
hndc9x <65893093+hndc9x@users.noreply.github.com>
2020-07-08 09:04:33 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,178 +1,37 @@
-import React from 'react';
-import {
-  StyleSheet,
-  View,
-  ImageBackground,
-  Text,
-  KeyboardAvoidingView,
-  Platform,
-  ActivityIndicator,
-  StatusBar,
-} from 'react-native';
+import * as React from 'react';
+import {  StyleSheet, Text, View } from "react-native";
 
-// Utils
-import { getLocationId, getWeather } from './utils/api';
-import getImageForWeather from './utils/getImageForWeather';
-import getIconForWeather from './utils/getIconForWeather';
-
-// Search component
-import SearchInput from './SearchInput';
-
-// MomentJS
-import moment from './moment';
-
-// CLASS
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // bind SCOPE
-    this.handleDate = this.handleDate.bind(this);
-
-    // STATE
-    this.state = {
-      loading: false,
-      error: false,
-
-      location: '',
-      temperature: 0,
-      weather: '',
-      created: '2000-01-01T00:00:00.000000Z'
-    };
-
-  }
-  // Life cycle
-  componentDidMount() {
-    this.handleUpdateLocation('Kiev');
-  }
-
-  // Parse of date
-  handleDate = date => moment(date).format("hh:mm:ss");
-
-  // Update current location
-  handleUpdateLocation = async city => {
-    if (!city) return;
-
-    this.setState({ loading: true }, async () => {
-      try {
-
-        const ID = await getLocationId(city);
-        const { location, weather, temperature, created } = await getWeather(ID);
-
-        this.setState({
-          loading: false,
-          error: false,
-          location,
-          weather,
-          temperature,
-          created,
-        });
-
-
-      } catch (e) {
-
-        this.setState({
-          loading: false,
-          error: true,
-        });
-
-      }
-    });
+  state = {
+    data:[]
   };
-
-  // RENDERING
+  componentDidMount(){
+    this.fetchData();
+  }
+  fetchData = async() =>{
+    const response = await fetch("https://nhom01-cloud-thayhuynhnam.herokuapp.com/iot/60.0");
+    const json = await response.json();
+    this.setState({
+      data: json.temp,
+      data1 : json.humidity
+    });
+  }
   render() {
-
-    // GET values of state
-    const { loading, error, location, weather, temperature, created } = this.state;
-
-    // Activity
     return (
-      <KeyboardAvoidingView style={styles.container} behavior="padding">
-
-        <StatusBar barStyle="light-content" />
-
-        <ImageBackground
-          source={getImageForWeather(weather)}
-          style={styles.imageContainer}
-          imageStyle={styles.image}
-        >
-
-          <View style={styles.detailsContainer}>
-
-            <ActivityIndicator animating={loading} color="white" size="large" />
-
-            {!loading && (
-              <View>
-                {error && (
-                  <Text style={[styles.smallText, styles.textStyle]}>
-                     Could not load your city or weather. Please try again later...
-                  </Text>
-                )}
-                {!error && (
-                  <View>
-                    <Text style={[styles.largeText, styles.textStyle]}>
-                      {getIconForWeather(weather)} {location}
-                    </Text>
-                    <Text style={[styles.smallText, styles.textStyle]}>
-                       {weather}
-                    </Text>
-                    <Text style={[styles.largeText, styles.textStyle]}>
-                      {`${Math.round(temperature)}°`}
-                    </Text>
-                  </View>
-                )}
-
-                <SearchInput
-                  placeholder="Search any city"
-                  onSubmit={this.handleUpdateLocation}
-                />
-
-                {!error && (
-                  <Text style={[styles.smallText, styles.textStyle]}>
-                    Last update: {this.handleDate(created)}
-                  </Text>
-                )}
-
-              </View>
-            )}
-          </View>
-        </ImageBackground>
-      </KeyboardAvoidingView>
+      <View style={styles.container}>
+        <Text> Thủ Đức - SPKT</Text>
+        <Text> Đô ẩm là : {this.state.data1}</Text>
+        <Text> Nhiệt độ là : {this.state.data}</Text>
+      </View>
     );
   }
 }
-
-/* StyleSheet */
 const styles = StyleSheet.create({
   container: {
+    marginTop: 15,
     flex: 1,
-    backgroundColor: '#34495E',
-  },
-  imageContainer: {
-    flex: 1,
-  },
-  image: {
-    flex: 1,
-    width: null,
-    height: null,
-    resizeMode: 'cover',
-  },
-  detailsContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: 'rgba(0,0,0,0.2)',
-    paddingHorizontal: 20,
-  },
-  textStyle: {
-    textAlign: 'center',
-    fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
-    color: 'white',
-  },
-  largeText: {
-    fontSize: 44,
-  },
-  smallText: {
-    fontSize: 18,
-  },
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  }
 });
